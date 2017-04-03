be67d68686690a4d7d4a4f87bd6b0d78eede68cd
WroJe <jrobinsonwrobel@2019.kehillah.org>
2017-04-03 09:17:57 -0700

    Initial commit. Create: Player, AI, Board, and Piece classes

---
+++ classes.py
@@ -0,0 +1,19 @@
+import random
+
+class Board:
+    """docstring for ."""
+    def __init__(self, arg):
+
+    """Board is 6 collumns 7 rows"""
+
+class Player:
+    """docstring for ."""
+    def __init__(self, arg):
+
+class AIplayer:
+    """docstring for ."""
+    def __init__(self, arg):
+
+class Piece:
+    """docstring for ."""
+    def __init__(self, arg):
