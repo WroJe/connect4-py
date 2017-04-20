67db6b736e16bb71d2fd819a34c11a528a9cc855
WroJe <jrobinsonwrobel@2019.kehillah.org>
2017-04-20 09:30:00 -0700

    Finish restructuring for  the day

--- classes.py
+++ classes.py
@@ -6,9 +6,17 @@ class Board:
     * Drawing game Board
     * Retain Piece info
     * Tracking Red and Yellow piece positions
-    * Board is 6 collumns 7 rows
+    * Board is 6 columns 7 rows
     """
     def __init__(self):
         self.boardColumns = 6
         self.boardRows = 7
         self.positions = {x: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], y: [ 1, 2, 3, 4, 5, 6]}
+
+    def drop(self, xAxis):
+
+
+class Piece:
+    """  """
+    def __init__(self):
+        self.pos = [][]
