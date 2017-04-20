81c2e6f7aa993273c10efdefa610f8eda5143302
WroJe <jrobinsonwrobel@2019.kehillah.org>
2017-04-20 09:18:16 -0700

    restructure classes. start def'ing board class vals

--- classes.py
+++ classes.py
@@ -6,48 +6,9 @@ class Board:
     * Drawing game Board
     * Retain Piece info
     * Tracking Red and Yellow piece positions
+    * Board is 6 collumns 7 rows
     """
     def __init__(self):
-        pass #get rid of this
-
-    boardColumns = 6
-    boardRows = 7
-    """Board is 6 collumns 7 rows"""
-
-    def tracking-Board(self):
-
-
-    def draw-Board(self)
-
-class Player:
-    """
-    responsibilities:
-    * Interfacing with the player(s)
-    * Tell the Board and Piece what the user wants to do
-    * Menus, etc.
-    """
-    def __init__(self):
-
-    """  """
-    def main-menu(self)
-        print("Welcome to... %s".center(80))
-        print("C O N N E C T  F O U R %s".center(80) % vcount)
-class Piece:
-    """
-    responsibilities:
-    * Taking Player input and telling the Board what to do with it
-    *
-    *
-    """
-    def __init__(self):
-
-    """  """
-
-class AIplayer:
-    """
-    docstring for AIplayer
-    implement this after game works
-    """
-    def __init__(self):
-
-    """  """
+        self.boardColumns = 6
+        self.boardRows = 7
+        self.positions = {x: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], y: [ 1, 2, 3, 4, 5, 6]}
