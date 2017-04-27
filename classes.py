fd4896e3a6da42ae598a4b9e603d5cad8b75be04
WroJe <jrobinsonwrobel@2019.kehillah.org>
2017-04-27 09:22:32 -0700

    update with Mr. K's code. restructure, again]

--- classes.py
+++ classes.py
@@ -3,29 +3,58 @@ import random
 class Board:
     """
     responsibilities:
-    * Drawing game Board
+
     * Retain Piece info
     * Tracking Red and Yellow piece positions
     * Board is 6 columns 7 rows
     """
     def __init__(self):
-        self.boardColumns = 6
+        # self.boardColumns = 6
         self.boardRows = 7
-        self.pos {
-            'xpos': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-            'ypos': [ 1, 2, 3, 4, 5, 6],
-            }
+
+        self.pieces = {
+            'a': [],
+            'b': [],
+            'c': [],
+            'd': [],
+            'f': [],
+            'g': []
+        }
+
+        # self.pos {
+        #     'xpos': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+        #     'ypos': [ 1, 2, 3, 4, 5, 6],
+        #     }
 
     def drop(self):
+        pass
 
+    def play(self, column, piece):
+        if len(self.board[column]) == self.boardRows:
+            return False
+        self.board[column].append(piece)
+        return True
 
 class Piece:
     """  """
-    def __init__(self):
-        self.pos = [][]
+    def __init__(self, color):
+        self.color = color
 
-    def place(self):
-        if
+
+class Player:
+
+    def __init__(self, color, board):
+        self.color = color
+        self.board = board
+
+    def take_turn(self):
+        choice = input('Which column do  you choose?')
+
+        if self.board.play(choice):
+            return
+        else:
+            print('Youo cant go there')
+            return self.take_turn()
 
 class Menu:
     def __init__(sels):
