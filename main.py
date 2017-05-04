f87e019f7b8e1d6dfc9ffcd6b37883db48e2b6d1
WroJe <jrobinsonwrobel@2019.kehillah.org>
2017-05-04 08:57:56 -0700

    create functioning menu (without exit option, yet)

--- main.py
+++ main.py
@@ -4,28 +4,32 @@ p1 = Player('yellow', b)
 p2 = Player('red', b)
 
 ill = Illustrator(b)
-
-def mainMenu(self):
+#gameWon = True
+def mainMenu():
     print("Welcome to Connect4.py!")
     print("written by Jeffrey 'wroje' Wrobel")
-    mainMenuInput = input(print("1. Play! \n 2. About"))
-        if mainMenuInput = 1:
-            gameWon = False
-        elif mainMenuInput = 2:
-            print("This program was written for KJHS 2nd Semester CompSci")
-            print()
-            print("License: Free for all personal use and modification.\n No commercial use. \n Attribute 'Jeffrey Wrobel' when using this code. ")
-            print("Credits \n Programming: Jeffrey Wrobel \n  Original Game: Howard Wexler, Ned Strongin,\n Special Thanks To: \n Mr. Daniel Kelley")
-            print("Connect4 is property of Hasbro Inc. ")
-            print()
-            print("clone the source code!")
-            print("https://github.com/WroJe/connect4-py.git")
-            print()
-            input(print("Press any key to continue...."))
-        else:
-            print("err")
+    print("1. Play!\n2. About")
+    mainMenuInput = input()
+    if mainMenuInput == '1':
+        gameWon == False
+    elif mainMenuInput == '2':
+        print("This program was written for KJHS 2nd Semester CompSci")
+        print()
+        print("License: Free for all personal use and modification.\nNo commercial use. \nAttribute 'Jeffrey Wrobel' when using this code. ")
+        print("Credits:\nProgramming: Jeffrey Wrobel\nOriginal Game: Howard Wexler, Ned Strongin,\nSpecial Thanks To: \nMr. Daniel Kelley")
+        print("Connect4 is property of Hasbro Inc. ")
+        print()
+        print("clone the source code!")
+        print("https://github.com/WroJe/connect4-py.git")
+        print()
+        print("Press any key to continue...")
+        menuReStart = input()
+        mainMenu()
+    else:
+        print("err")
 
-while gameWon = False:
+mainMenu()
+while gameWon == False:
     ill.draw()
     p1.take_turn()
     b.check_win()
